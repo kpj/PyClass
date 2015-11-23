2f168fa2886a4e3c00f15b5407bb860f0f9b38f4
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2015-11-23 18:10:04 +0100

    Add ability to test model on all images in some directory

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 Classify images using random decision forests
 """
 
-import sys
+import sys, os
 
 from mlearner import train_model
 from utils import get_feature_vector
@@ -12,15 +12,37 @@ def classify_image(image_path, clf):
     """ Classify given image
     """
     fvecs = [get_feature_vector(image_path)]
-    print(clf.predict(fvecs))
+    print(clf.predict(fvecs), clf.predict_proba(fvecs))
+
+def compute_score(root_dir, clf):
+    """ Test model with all images in given directory
+    """
+    fvecs = []
+    true_classes = []
+
+    for fn in os.listdir(root_dir):
+        assert len(fn.split('_')) > 1, 'Invalid filename'
+        fname = os.path.join(root_dir, fn)
+
+        fvecs.append(get_feature_vector(fname))
+        true_classes.append(fn.split('_')[0])
+
+    print('Accuracy:', clf.score(fvecs, true_classes))
 
 def main():
     if len(sys.argv) != 2:
-        print('Usage: %s <image>' % sys.argv[0])
+        print('Usage: %s <image|directory>' % sys.argv[0])
         sys.exit(1)
 
     clf = train_model()
-    classify_image(sys.argv[1], clf)
+
+    arg = sys.argv[1]
+    if os.path.isfile(arg):
+        classify_image(arg, clf)
+    elif os.path.isdir(arg):
+        compute_score(arg, clf)
+    else:
+        print('Invalid argument...')
 
 
 if __name__ == '__main__':
